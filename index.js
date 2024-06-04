da660f21db5fd7ed69c660931677e1f4247f061c
Alan <alanitoyah96@gmail.com>
2024-06-03 23:14:28 -0500

    rendered the x button to close pop up

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const modalEl = document.getElementById('modal')
 
+const modalCloseBtn = document.getElementById('modal-close-btn')
+
 setTimeout(function(){
     modalEl.style.display = 'inline'
  }, 1500) //1.5 seconds
+
+ modalCloseBtn.addEventListener("click", function(){
+    modalEl.style.display = 'none'
+ })
