24cec3c985b87e6382f6414b1b8b6dd07f59084b
Alan <alanitoyah96@gmail.com>
2024-06-08 17:59:34 -0500

    stopped defualt submit method of html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const modalEl = document.getElementById('modal')
 
 const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById('consent-form')
 
 setTimeout(function(){
     modalEl.style.display = 'inline'
@@ -8,4 +9,9 @@ setTimeout(function(){
 
  modalCloseBtn.addEventListener("click", function(){
     modalEl.style.display = 'none'
+ })
+
+ consentForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    console.log('form submitted')
  })
